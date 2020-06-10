b7031920391de6985dcfcbddd3391ef62513bd72
taras.rushchak <taras.rushchak@dreamscapenetworks.com>
2020-06-10 20:33:24 +0300

    [master] - Fetching and Rendering props

--- blog/client/src/PostList.jsx
+++ blog/client/src/PostList.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const PostList = () => {
+
+    const [posts, setPosts] = useState({})
+
+    const fetchPosts = async () => {
+        const response = await axios.get('http://localhost:4000/posts')
+
+        setPosts(response.data)
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+
+
     return (
-        <div>
-            
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {Object.values(posts).map((post) => {
+                return (
+                    <h1 key={post.id}>{post.title}</h1>
+                )
+            })}
         </div>
     );
 };
