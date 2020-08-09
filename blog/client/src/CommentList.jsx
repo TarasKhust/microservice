3f08925ead8ddbc09bb2486fec33215f9185c14a
taras.rushchak <taras.rushchak@dreamscapenetworks.com>
2020-08-09 21:22:42 +0300

    [master] - using the query service lesson 31

--- blog/client/src/CommentList.jsx
+++ blog/client/src/CommentList.jsx
@@ -1,19 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
+import React from 'react';
 
-const CommentList = ({postId}) => {
+const CommentList = ({comments}) => {
 
-    const [comments, setComments] = useState([])
-
-    const fetchData = async () => {
-        const response = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-        setComments(response.data)
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
 
     return (
         <div>
