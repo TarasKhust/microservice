c1c36918bd74284ecc2fb4b2ec74e5a11894468a
taras.rushchak <taras.rushchak@dreamscapenetworks.com>
2020-08-17 21:04:12 +0300

    [master] - lesson 38

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -5,7 +5,24 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (reg, res) => {
+app.post('/events', async (reg, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+
+    res.send({})
 
 })
 
