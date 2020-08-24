9b3736bde36c866bbb3fcb0c5a2a447ca6c35ce6
taras.rushchak <taras.rushchak@dreamscapenetworks.com>
2020-08-24 19:51:45 +0300

    [master] - lesson 41

--- blog/client/src/CommentList.jsx
+++ blog/client/src/CommentList.jsx
@@ -7,6 +7,19 @@ const CommentList = ({comments}) => {
         <div>
 
             {comments.map((comment) => {
+                let content;
+                if (comment.status = 'approved') {
+                    content = comment.content;
+                }
+
+                if (comment.status = 'pending') {
+                    content = "This comment is awaiting moderation"
+                }
+
+                if (comment.status = 'rejected') {
+                    content = "This comment has been rejected"
+                }
+
                 return (
                     <ul key={comment.id}>
                         <li key={comment.id}>{comment.content}</li>
